0aadd5f670e4c5f52f8ef80137675d034bc63bf4
dcodemonkey <nirmeshrahul@gmail.com>
2019-02-19 15:21:58 +0530

    Adding Calendar/DatePicker to our DialogActivity

--- app/src/main/java/com/android/rahul/nirmesh/communicatingwithuser/DialogActivity.java
+++ app/src/main/java/com/android/rahul/nirmesh/communicatingwithuser/DialogActivity.java
@@ -1,12 +1,17 @@
 package com.android.rahul.nirmesh.communicatingwithuser;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 
 import com.android.rahul.nirmesh.communicatingwithuser.Dialogs.CustomDialogFragment;
 import com.android.rahul.nirmesh.communicatingwithuser.Dialogs.SimpleDialogFragment;
 import com.android.rahul.nirmesh.communicatingwithuser.Dialogs.SingleChoiceDialogFragment;
+
+import java.util.Calendar;
 
 public class DialogActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -32,11 +37,19 @@ public class DialogActivity extends AppCompatActivity implements View.OnClickLis
                 break;
             case R.id.btnShowDatePicker:
                 // TODO: Get a calendar instance
+                Calendar calendar = Calendar.getInstance();
 
                 // TODO: Create a DatePickerDialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        Log.i(TAG, String.format("Date chosen: day: %d, month: %d, year: %d", day, month + 1, year));
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
 
                 // TODO: Set the title and show the dialog
-
+                datePickerDialog.setTitle("Choose a Date");
+                datePickerDialog.show();
                 break;
             case R.id.btnShowChoiceDialog:
                 showChoiceDialog();
