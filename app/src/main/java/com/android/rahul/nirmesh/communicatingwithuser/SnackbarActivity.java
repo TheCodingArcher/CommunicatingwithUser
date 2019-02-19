63cfc587167e50d0e2f25de7e156ad4e7de86e0a
dcodemonkey <nirmeshrahul@gmail.com>
2019-02-19 13:52:36 +0530

    Snackbar Initial Code

--- app/src/main/java/com/android/rahul/nirmesh/communicatingwithuser/SnackbarActivity.java
+++ app/src/main/java/com/android/rahul/nirmesh/communicatingwithuser/SnackbarActivity.java
@@ -2,12 +2,40 @@ package com.android.rahul.nirmesh.communicatingwithuser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SnackbarActivity extends AppCompatActivity {
+public class SnackbarActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String TAG="AUC-SNACKBAR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snackbar);
+
+        findViewById(R.id.btnShowSnackbar).setOnClickListener(this);
+        findViewById(R.id.btnShowActionSnackbar).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnShowSnackbar:
+                showSnackbar();
+                break;
+            case R.id.btnShowActionSnackbar:
+                showActionSnackbar();
+                break;
+        }
+    }
+
+    private void showSnackbar() {
+        // TODO: Create and show the snackbar
+
+    }
+
+    private void showActionSnackbar() {
+        // TODO: Create a snackbar that has an action item
+
     }
 }
